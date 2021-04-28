3c38f7b58c65a92e96383f0b6c693f34d201fdde
Vaibhav Gaikwad <gaikwadvaibhav03@gmail.com>
2021-04-28 20:48:18 +0530

    added amchart and canvasjs line chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,101 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+am4core.useTheme(am4themes_animated);
+
+import * as CanvasJS from '../assets/canvasjs.min';  // Canvasjs dependacy imported
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chars-demo';
+export class AppComponent implements AfterViewInit {
+  title = 'charts-demo';
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+   this.loadAmchart()
+    //   ------------------- Canvasjs line char -----------------
+    this.loadCanvasChart();
+  }
+
+  loadAmchart() {
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+    // Add data
+    chart.data = [{
+      "date": new Date(2018, 3, 20),
+      "value": 90
+    }, {
+      "date": new Date(2018, 3, 21),
+      "value": 102
+    }, {
+      "date": new Date(2018, 3, 22),
+      "value": 65
+    }, {
+      "date": new Date(2018, 3, 23),
+      "value": 62
+    }, {
+      "date": new Date(2018, 3, 24),
+      "value": 55
+    }, {
+      "date": new Date(2018, 3, 25),
+      "value": 81
+    }];
+
+    // Create axes
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+
+    // Create value axis
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    // Create series
+    let lineSeries = chart.series.push(new am4charts.LineSeries());
+    lineSeries.dataFields.valueY = "value";
+    lineSeries.dataFields.dateX = "date";
+    lineSeries.name = "Sales";
+    lineSeries.strokeWidth = 3;
+
+    // Add simple bullet
+    let bullet = lineSeries.bullets.push(new am4charts.Bullet());
+    let image = bullet.createChild(am4core.Image);
+    image.href = "https://www.amcharts.com/lib/images/star.svg";
+    image.width = 30;
+    image.height = 30;
+    image.horizontalCenter = "middle";
+    image.verticalCenter = "middle";
+  }
+
+  loadCanvasChart() {
+    let charts = new CanvasJS.Chart("chartContainer",
+      {
+        title: {
+          text: "Custom Marker on dataPoint"
+        },
+        data: [
+          {
+            type: "line",
+            showInLegend: true,
+            dataPoints: [
+              { x: 10, y: 71, indexLabel: "71", markerType: "circle", markerColor: "grey", markerSize: 12 },
+              { x: 20, y: 55, indexLabel: "55", markerType: "circle", markerColor: "grey", markerSize: 12 },
+              { x: 30, y: 50, indexLabel: "50", markerType: "circle", markerColor: "grey", markerSize: 12 },
+              { x: 40, y: 65, indexLabel: "65", markerType: "circle", markerColor: "grey", markerSize: 12 },
+              { x: 50, y: 85, indexLabel: "85", markerType: "triangle", markerColor: "red", markerSize: 12 },
+              { x: 60, y: 68, indexLabel: "68", markerType: "circle", markerColor: "grey", markerSize: 12 },
+              { x: 70, y: 28, indexLabel: "28", markerType: "circle", markerColor: "grey", markerSize: 12 },
+              { x: 80, y: 34, indexLabel: "34", markerType: "circle", markerColor: "grey", markerSize: 12 },
+              { x: 90, y: 24, indexLabel: "24", markerType: "circle", markerColor: "grey", markerSize: 12 }
+            ]
+          }
+        ]
+      });
+
+    charts.render();
+  }
+
+
 }
